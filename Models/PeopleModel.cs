b314af7a86e5221b5e8b1693b220656e7af00ec4
jess voss <jess_wolf_24@hotmail.com>
2020-03-03 01:52:18 -0500

    Add project files.

---
+++ Models/PeopleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTestCompany.Models
+{
+    public class PeopleModel
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string title { get; set; }
+        public string depNumber { get; set; }
+        public char type { get; set; } = 's';
+        public bool isActive { get; set; } = true;
+    }
+}
